4d110879b4630a074e008bcee4acc34199e0d3e6
Darryl Wright <darryl@punkscience.ca>
2026-02-06 15:11:32 -0400

    Fix Chocolatey uninstall script

--- chocolatey/tools/chocolateyUninstall.ps1
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -7,4 +7,5 @@ Uninstall-BinFile -Name 'ListenRelay'
 $shortcutPath = Join-Path $env:ProgramData "Microsoft\Windows\Start Menu\Programs\ListenRelay.lnk"
 if (Test-Path $shortcutPath) { Remove-Item $shortcutPath -Force }
 
-Uninstall-ChocolateyZipPackage -PackageName $packageName
+# No explicit Uninstall-ChocolateyZipPackage exists/is needed 
+# Chocolatey removes the tools directory automatically
